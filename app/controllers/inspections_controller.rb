98cd8ea5d2063cbcfb2a169018c5efdb65c58bdd
Alperen Bozkurt <bozkurttalperen@gmail.com>
2019-03-31 18:59:37 +0300

    Yoklama alma endpointinde id ile ders seçebilme eklendi

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -1,16 +1,19 @@
 class InspectionsController < ApplicationController
     def create
-        @inspection = Inspection.new
-        @inspection.image.attach(params[:image])
-        @inspection.lesson = Lesson.first
-        @inspection.student = Student.first
-        if @inspection.save!
-            render json: { message: "Yoklama Başarıyla alındı." }
-            active_storage_disk_service = ActiveStorage::Service::DiskService.new(root: Rails.root.to_s + '/storage/')
-            puts active_storage_disk_service.send(:path_for, @inspection.image.blob.key)
-        else
-            render json: { message: "Hata Oluştu." }
-        end
+      lessons = Lesson.pluck :id
+      lesson_id = lessons[params["lesson_number"].to_i]
+
+      @inspection = Inspection.new
+      @inspection.image.attach(params[:image])
+      @inspection.lesson = Lesson.find(lesson_id)
+      @inspection.student = Student.first
+      if @inspection.save!
+        render json: { message: "Yoklama Başarıyla alındı." }
+        active_storage_disk_service = ActiveStorage::Service::DiskService.new(root: Rails.root.to_s + '/storage/')
+        puts active_storage_disk_service.send(:path_for, @inspection.image.blob.key)
+      else
+        render json: { message: "Hata Oluştu." }
+      end
     end
     def show
         @inspection = Inspection.find(params[:id])
